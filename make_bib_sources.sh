191a5775c11c651785e94e1b65e426821080ce5d
bschroed <m-bsGETH1111>
2021-01-14 15:45:37 +0100

    clean even further

--- make_bib_sources.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo "./frontback/acknowledgements.tex ./frontback/abstract.tex ./frontback/publications.tex" > sources.dat
-for i in intro `seq 1 4` outlook; do 
-    printf "%s " "chapter_${i}/content.tex"; 
-    cat chapter_${i}/content.tex | grep input |  awk -F "[{} /]" -v path=chapter_${i} '{for (i=1; i <= NF; i+=1 ) {if ( $i ~ ".tex") {printf "%s ", path"/"$(i-1)"/"$i;};};}'; 
-done >> ./sources.dat
-echo "./frontback/cv.tex" >> sources.dat
-cat $( cat ./sources.dat )
